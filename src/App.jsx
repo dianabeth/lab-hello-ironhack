1f88e83025ec512cb66c4819b0532a6d7504f9dc
Diana Odiaka-Obiagwu <59796234+dianabeth@users.noreply.github.com>
2020-08-10 16:40:04 +0100

    working on it

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,40 @@ import logo from './logo.svg';
 
 import './App.css';
 
+const message = 'Say hello to ReactJS';
+
+function BodyComponent() {
+  return (
+    <div>
+      <h1>{message}</h1>
+      <p>You will learn a Frontend Framework from scratch to become a Ninja Developer.</p>
+    </div>
+  );
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <body>
+          <header className="App-header">
+            <nav>
+              <img src="./images/ironhack-logo.svg" className="App-logo" alt="ironhack.logo" />
+              <img src="./images/menu-top.svg" className="nav" alt="dropdown-bar" />
+            </nav>
+            <BodyComponent />
+          </header>
+
+          <footer>
+            <div>
+              <img src="./images/icon1.png" alt="icon" />
+              <img src="./images/icon2.png" alt="icon" />
+              <img src="./images/icon3.png" alt="icon" />
+              <img src="./images/icon4.png" alt="icon" />
+              <img src="./images/icon5.png" alt="icon" />
+            </div>
+          </footer>
+        </body>
       </div>
     );
   }
